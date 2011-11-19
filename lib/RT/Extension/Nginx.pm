0228e694b92cbcd51a4abc3b44a10ec4b8c4a29a
Ruslan Zakirov <ruz@bestpractical.com>
2011-11-20 02:39:33 +0400

    initial commit

---
+++ lib/RT/Extension/Nginx.pm
@@ -0,0 +1,27 @@
+package RT::Extension::Nginx;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT::Extension::Nginx - build a 
+
+=head1 DESCRIPION
+
+=cut
+
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>ruz@bestpractical.comE<gt>
+
+=head1 LICENSE
+
+Under the same terms as perl itself.
+
+=cut
+
+1;
